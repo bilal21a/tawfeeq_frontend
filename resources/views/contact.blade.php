6d5dbbc779a5a6a673c00951d649b5bfa810b37f
Bilal Anwar <b.anwar21.a@gmail.com>
2023-09-04 23:47:29 +0500

    project

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html lang="en" data-footer="true" data-scrollspy="true" dir="rtl">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <title>Acorn Admin Template | Form Layouts</title>
+    <meta name="description" content="Examples and usage guidelines for form control styles and layout options." />
+    <!-- Favicon Tags Start -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link rel="apple-touch-icon-precomposed" sizes="57x57" href="img/favicon/apple-touch-icon-57x57.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="img/favicon/apple-touch-icon-114x114.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="img/favicon/apple-touch-icon-72x72.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="img/favicon/apple-touch-icon-144x144.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="60x60" href="img/favicon/apple-touch-icon-60x60.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="120x120" href="img/favicon/apple-touch-icon-120x120.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="76x76" href="img/favicon/apple-touch-icon-76x76.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="152x152" href="img/favicon/apple-touch-icon-152x152.png" />
+    <link rel="icon" type="image/png" href="img/favicon/favicon-196x196.png" sizes="196x196" />
+    <link rel="icon" type="image/png" href="img/favicon/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/png" href="img/favicon/favicon-32x32.png" sizes="32x32" />
+    <link rel="icon" type="image/png" href="img/favicon/favicon-16x16.png" sizes="16x16" />
+    <link rel="icon" type="image/png" href="img/favicon/favicon-128.png" sizes="128x128" />
+    <meta name="application-name" content="&nbsp;" />
+    <meta name="msapplication-TileColor" content="#FFFFFF" />
+    <meta name="msapplication-TileImage" content="img/favicon/mstile-144x144.png" />
+    <meta name="msapplication-square70x70logo" content="img/favicon/mstile-70x70.png" />
+    <meta name="msapplication-square150x150logo" content="img/favicon/mstile-150x150.png" />
+    <meta name="msapplication-wide310x150logo" content="img/favicon/mstile-310x150.png" />
+    <meta name="msapplication-square310x310logo" content="img/favicon/mstile-310x310.png" />
+    <!-- Favicon Tags End -->
+    <!-- Font Tags Start -->
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;400;700&display=swap" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="font/CS-Interface/style.css" />
+    <!-- Font Tags End -->
+    <!-- Vendor Styles Start -->
+    <link rel="stylesheet" href="css/vendor/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/vendor/OverlayScrollbars.min.css" />
+
+    <link rel="stylesheet" href="css/vendor/select2.min.css" />
+
+    <link rel="stylesheet" href="css/vendor/select2-bootstrap4.min.css" />
+
+    <link rel="stylesheet" href="css/vendor/bootstrap-datepicker3.standalone.min.css" />
+
+    <link rel="stylesheet" href="css/vendor/tagify.css" />
+
+    <!-- Vendor Styles End -->
+    <!-- Template Base Styles Start -->
+    <link rel="stylesheet" href="css/styles.css" />
+    <!-- Template Base Styles End -->
+
+    <link rel="stylesheet" href="css/main.css" />
+    <script src="js/base/loader.js"></script>
+
+</head>
+
+<body class="letter-spacing">
+    <div id="root">
+
+        <main>
+            <div id="root" class="pt-md-7">
+                <div class="row">
+                    <div class="col-xl-2 col-lg-2 col-md-2 col-sm-0"></div>
+                    <div class="col-xl-8 col-lg-8 col-md-8 col-sm-12 me-md-7">
+                        <div class="container">
+                            <div class="page-title-container">
+                                <div class="mb-5 mt-5">
+                                    <div class="row custom-row">
+                                        <div class="col-xl-5 col-lg-5 col-md-5 col-sm-12 pb-3 p-sm-0">
+                                            <div class="card">
+                                                <div class="card-body pb-7">
+                                                    <div class="ps-4 pb-7">
+                                                        <h5 class="fw-semibold color_green mb-0">تواصل معنا</h5><br>
+                                                        <div class="d-flex"> <i data-acorn-icon="phone" class="ms-2" data-acorn-size="17"></i>
+                                                            <p>+971 55 505 5911</p>
+                                                        </div>
+                                                        <div class="d-flex"> <i data-acorn-icon="email" class="ms-2" data-acorn-size="17"></i>
+                                                            <p>info@tawafeeq.com</p>
+                                                        </div>
+                                                        <div class="d-flex"> <i data-acorn-icon="pin" class="ms-2" data-acorn-size="17"></i>
+                                                            <p>الامارات - دبي</p>
+                                                        </div>
+                                                        <h5 class="fw-semibold text-primary mb-0">
+                                                            تابعنا على مواقع التواصل الاجتماعي
+                                                        </h5><br>
+                                                        <div class="pt-3">
+                                                            <i data-acorn-icon="facebook" class="ms-2" data-acorn-size="24"></i>
+                                                            <i data-acorn-icon="twitter" class="ms-2" data-acorn-size="24"></i>
+                                                            <i data-acorn-icon="instagram" class="ms-2" data-acorn-size="24"></i>
+                                                        </div>
+
+                                                    </div>
+
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-xl-5 col-lg-5 col-md-5 col-sm-12 p-sm-0">
+                                            <div class="card">
+                                                <div class="card-body pb-1">
+                                                    <div class="ps-4 pb-2">
+                                                        <h5 class="fw-semibold color_green mb-0">أرسل رسالة</h5><br>
+                                                        <form action="">
+                                                            <p class="fw-bolder fs-6 text-black d-flex justify-content-start">الموضوع:*
+                                                            </p>
+                                                            <input type="text" name="topic" id="topic" class="form-control input_background">
+                                                            <p class="fw-bolder fs-6 text-black d-flex justify-content-start">المحتوى:*
+                                                            </p>
+                                                            <textarea class="form-control form-control-sm  input_background" name="content" id="" cols="30" rows="5"></textarea>
+
+                                                            <input class="btn color_green_button mt-4" type="submit" value="تسجيل الدخول" style="background-color: #1ED67A;">
+                                                        </form>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-2 col-lg-2 col-md-2 col-sm-0"></div>
+                    </div>
+                    <!-- Layout Footer End -->
+                </div>
+        </main>
+        <!-- Search Modal End -->
+
+        <!-- Vendor Scripts Start -->
+        <script src="js/vendor/jquery-3.5.1.min.js"></script>
+        <script src="js/vendor/bootstrap.bundle.min.js"></script>
+        <script src="js/vendor/OverlayScrollbars.min.js"></script>
+        <script src="js/vendor/autoComplete.min.js"></script>
+        <script src="js/vendor/clamp.min.js"></script>
+
+        <script src="icon/acorn-icons.js"></script>
+        <script src="icon/acorn-icons-interface.js"></script>
+
+        <script src="js/cs/scrollspy.js"></script>
+
+        <script src="js/vendor/select2.full.min.js"></script>
+
+        <script src="js/vendor/datepicker/bootstrap-datepicker.min.js"></script>
+
+        <script src="js/vendor/tagify.min.js"></script>
+
+        <!-- Vendor Scripts End -->
+
+        <!-- Template Base Scripts Start -->
+        <script src="js/base/helpers.js"></script>
+        <script src="js/base/globals.js"></script>
+        <script src="js/base/nav.js"></script>
+        <script src="js/base/search.js"></script>
+        <script src="js/base/settings.js"></script>
+        <!-- Template Base Scripts End -->
+        <!-- Page Specific Scripts Start -->
+
+        <script src="js/forms/layouts.js"></script>
+
+        <script src="js/common.js"></script>
+        <script src="js/scripts.js"></script>
+        <!-- Page Specific Scripts End -->
+</body>
+
+</html>
---
+++ resources/views/layouts/partials/footer.blade.php
@@ -0,0 +1,60 @@
+<footer class="footer">
+    <div class="row d-flex justify-content-center p-7" style="width: 99% !important;">
+        <!-- Large screens: 3 columns, each col-3 -->
+        <div class="col-lg-3 col-md-4 col-sm-6 col-12 mb-4 line_height_footer">
+            <h4 class="white fw-bold mb-6">احصل على المساعدة</h4>
+            <div>
+                <a class="white fw-bold text_decoration" href="#">
+                    الاسئلة الأكثر شيوعا
+                </a>
+            </div>
+            <div>
+                <a class="white fw-bold text_decoration font_size" href="about.html">
+                من نحن
+                </a>
+            </div>
+            <div>
+                <a class="white fw-bold text_decoration font_size" href="pricing.html">
+                    الباقات
+                </a>
+            </div>
+            <div>
+                <a class="white fw-bold text_decoration font_size" href="pricing.html">
+                    خدمات
+                </a>
+            </div>
+            <div>
+                <a class="white fw-bold text_decoration font_size" href="pricing.html">
+                    المدونة
+                </a>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-4 col-sm-6 col-12 mb-4 line_height_footer">
+            <h4 class="white fw-bold mb-6"> أعطنا رأيك</h4>
+            <textarea class="form-control blue_background" name="" id="" cols="25" rows="10" placeholder=" ما رأيك في موقع تَوَافِيْق " style="color:#fff;"></textarea>
+            <button type="button" class="btn btn-primary mb-1 mt-4">Primary</button>
+        </div>
+        <div class="col-lg-3 col-md-4 col-sm-6 col-12 mb-4 line_height_footer">
+            <h4 class="white fw-bold mb-6">تواصل معنا</h4>
+            <div class="ps-4 pb-7 white">
+                <div class="d-flex ">
+                    <i data-acorn-icon="phone" class="ms-2" data-acorn-size="17"></i>
+                    <p>+971 55 505 5911</p>
+                </div>
+                <div class="d-flex"> <i data-acorn-icon="email" class="ms-2" data-acorn-size="17"></i>
+                    <p>info@tawafeeq.com</p>
+                </div>
+                <div class="d-flex"> <i data-acorn-icon="pin" class="ms-2" data-acorn-size="17"></i>
+                    <p>الامارات - دبي</p>
+                </div>
+
+                <div class="pt-3">
+                    <i data-acorn-icon="facebook" class="ms-2" data-acorn-size="24"></i>
+                    <i data-acorn-icon="twitter" class="ms-2" data-acorn-size="24"></i>
+                    <i data-acorn-icon="instagram" class="ms-2" data-acorn-size="24"></i>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</footer>
